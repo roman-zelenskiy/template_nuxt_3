f0efbf3001fea75cfa6b6e8d957d43cc86ca97a9
Roman <romazelenskiy2000@gmail.com>
2024-04-27 12:11:14 +0300

    fix template

---
+++ components/ui/Button.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  defineProps<{
+    to?: string;
+    type?: 'button' | 'submit' | 'reset' | undefined;
+  }>();
+</script>
+
+<template>
+  <nuxtLink v-if="to" class="focus:outline-none focus:outline-0 disabled:opacity-100" :to="to">
+    <slot />
+  </nuxtLink>
+  <button
+    v-else
+    class="focus:outline-none focus:outline-0 disabled:opacity-100"
+    :type="type || 'button'"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped></style>
